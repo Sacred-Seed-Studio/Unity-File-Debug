7c09482d126602c8ddf2cd6fc12aeddcf7469e50
Joel Kuntz <joel.kuntz.92@gmail.com>
2016-03-06 12:53:02 -0400

    Initial version

---
+++ Assets/UnityFileDebug/Demo/Tester.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Tester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Test", DLogType.AI);
+
+        Debug.Log("Test with a super dooper long name akjlsdf klajsd lfkjads flkjasd flkjasdf ljkasdl fkjasl kfjasdlkf jasl fjaslk fjasldjf klasjfd lkasjdf klasjfd klsjadf klasjfd lkasjdf klasjdf ;lkasjfd lksajd fk", DLogType.Log);
+        Debug.LogFormat("<color=green>This is a green message!</color>");
+	}
+}
---
+++ Assets/UnityFileDebug/README.txt
@@ -0,0 +1,17 @@
+********************************
+********** HOW TO USE **********
+********************************
+Copy the Unity File Debug prefab into your scene. 
+Use Debug.Log as normal. 
+
+See full README for more details
+https://github.com/Sacred-Seed-Studio/Unity-File-Debug/blob/master/README.md
+
+*********************************
+
+Unity File Debugger
+MIT Licensed
+
+Source: https://github.com/Sacred-Seed-Studio/Unity-File-Debug
+
+Bugs / Feature Requests: https://github.com/Sacred-Seed-Studio/Unity-File-Debug/issues
